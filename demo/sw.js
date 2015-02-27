04ca9ef88e10631bb6d78fa331f4d4424abba4bb
Francisco Jordano <fjordano@mozilla.com>
2015-02-27 14:57:59 +0000

    Allow middleware being just functions for hadling 'fetch' events to make it closer to express

--- demo/sw.js
+++ demo/sw.js
@@ -1,4 +1,3 @@
-//importScripts('../lib/sww.js');
 importScripts('../dist/sww.0.0.0.min.js');
 
 var worker = new self.ServiceWorkerWare();
@@ -16,7 +15,29 @@ var SimpleWare = {
   }
 };
 
+// Simple middleware that just listen to SW livecycle events
 worker.use(SimpleWare);
+
+// We precache known resources with the StaticCacher middleware
 worker.use(new self.StaticCacher(['a.html']));
+
+// Middleware example for handling 'virtual' urls and building the
+// response with js.
+worker.get('virtual/.', function(request, response) {
+  var url = request.clone().url;
+
+  var content = '<html><body>'
+      + url + ' ' + Math.random()
+      + '<br/><a href="/demo/index.html">index</a></body></html>';
+
+  return Promise.resolve(new Response(content, {
+    headers: {
+      'Content-Type': 'text/html'
+    }
+  }));
+});
+
+// Handles offline resources saved by the StaticCacher middleware,
+// also caches those resources not in the cache for next visit
 worker.use(new self.SimpleOfflineCache());
 worker.init();
