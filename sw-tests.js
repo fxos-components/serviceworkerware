b7e9607059630bc52d8286fe91129aa39cd014b7
Johan Lorenzo <jlorenzo@mozilla.com>
2015-04-30 15:00:48 +0200

    Bug 1160135 - Add tests to staticcacher

--- sw-tests.js
+++ sw-tests.js
@@ -2,5 +2,6 @@
 var SW_TESTS = [
   '/base/lib/spec/router.sw-spec.js',
   '/base/lib/spec/simpleofflinecache.sw-spec.js',
-  '/base/lib/spec/sww.sw-spec.js'
+  '/base/lib/spec/sww.sw-spec.js',
+  '/base/lib/spec/staticcacher.sw-spec.js'
 ];
