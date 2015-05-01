f80823c682696f270595c000302558559be45d99
Francisco Jordano <fjordano@mozilla.com>
2015-05-01 18:05:23 +0100

    Enabling tests for ChromeCanary

--- testing/karma-sw.conf.js
+++ testing/karma-sw.conf.js
@@ -55,7 +55,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: [/*'ChromeCanary',*/'NightlySW'],
+    browsers: ['ChromeCanary', 'NightlySW'],
 
     customLaunchers: {
       'NightlySW': {
