54b05dd44aa2896857a9c88da8cc182a7d253371
Francisco Jordano <fjordano@mozilla.com>
2015-05-29 13:45:14 +0100

    Wrapping the default mw with a decorator to stopIfResponse Adding latest comments

--- sw-tests.js
+++ sw-tests.js
@@ -1,6 +1,6 @@
 
 var SW_TESTS = [
-  // '/base/lib/spec/router.sw-spec.js',
-  // '/base/lib/spec/simpleofflinecache.sw-spec.js',
+  '/base/lib/spec/router.sw-spec.js',
+  '/base/lib/spec/simpleofflinecache.sw-spec.js',
   '/base/lib/spec/sww.sw-spec.js'
 ];
